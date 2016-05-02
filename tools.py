67a10d02894c537dd2fc4f8aa3e760ea31b7bb71
Isaiah Mann <imann24@gmail.com>
2016-05-02 14:43:18 -0400

    Correcting spelling errors (cont)

--- tools.py
+++ tools.py
@@ -7,14 +7,14 @@ def generate_cipher (ascii_limit = 128):
 	for i in range(0, ascii_limit):
 		char_set.append(chr(i))
 	random.shuffle(char_set)
-	cypher = open(cypher_name, 'w')
+	cipher = open(cyiher_name, 'w')
 	for i in range(0, ascii_limit):
 		cypher.write(chr(i) + char_set[i])
 		if (i < ascii_limit - 1):
-			cypher.write("\n")
-	cypher.close()
+			cipher.write("\n")
+	cipher.close()
 
-generate_cypher()
+generate_cipher()
 
 def encrypt (file_path):
 	print("Encrypting")
